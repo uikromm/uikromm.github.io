8021f470bc3d6d581ce8274a2c8aa814772c6eab
uikromm <u.ikromm@gmail.com>
2025-03-03 06:33:11 +0500

    added media

--- src/db/msg.ts
+++ src/db/msg.ts
@@ -2,10 +2,6 @@ interface Imsgs {
     cv_txt: string;
     greeting_txt: string;
     about_txt: string;
-    all_skills: {
-        title: string;
-        skills?: string;
-    }[];
     social_links: {
         title: string;
         href: string;
@@ -16,7 +12,6 @@ export const msgs: Imsgs = {
     cv_txt: "Download CV",
     greeting_txt: "Hi, I'm Ikrom 👋",
     about_txt: "I'm a <b>frontend developer</b> with over 4 years of web experiences. I work with <b>React.js</b> and <b>Next.js</b>. Currently, I am broadening my knowledge in back-end development.",
-    all_skills: [],
     social_links: [
         {
             title: "GitHub",
